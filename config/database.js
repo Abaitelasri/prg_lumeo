9c595062f1378cc9abca29a2b0b56edf15c3ef83
Anas Agouriane <anas.agouriane@it.devcorp.fr>
2023-05-21 15:13:59 +0100

    az

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const { Model } = require('objection');
-const Knex = require('knex');
+const knex = require('knex')('production')
 const knexConfig = require('./knexfile');
 
-const knex = Knex(knexConfig[process.env.NODE_ENV]);
 
 Model.knex(knex);
 
